08b6abef02f65df5b5a41a647a1491c5368aa576
Aleksey Smirnov <lirien42@gmail.com>
2018-01-10 10:19:28 +0300

    rename function makeDescIfObject to valueToString

--- src/formats/formatPlain.js
+++ src/formats/formatPlain.js
@@ -1,18 +1,18 @@
 import _ from 'lodash';
 
-const makeDescIfObject = (value, prefix = '', simpleStr = 'complex value') => (_.isObject(value) ? simpleStr : `${prefix}'${value}'`);
+const valueToString = (value, prefix = '', simpleStr = 'complex value') => (_.isObject(value) ? simpleStr : `${prefix}'${value}'`);
 
 const formatAdapters = {
   nested: (node, path, formatAst) => formatAst(node.children, `${path}${node.name}.`),
   notchanged: () => {},
   changed: (node, path) => {
-    const oldValue = makeDescIfObject(node.oldValue);
-    const newValue = makeDescIfObject(node.newValue);
+    const oldValue = valueToString(node.oldValue);
+    const newValue = valueToString(node.newValue);
     return `Property '${path}${node.name}' was updated From ${oldValue} to ${newValue}`;
   },
   deleted: (node, path) => `Property '${path}${node.name}' was removed`,
   added: (node, path) => {
-    const newValue = makeDescIfObject(node.newValue, 'value: ');
+    const newValue = valueToString(node.newValue, 'value: ');
     return `Property '${path}${node.name}' was added with ${newValue}`;
   },
 };
